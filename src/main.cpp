d6762be63f9948a02b04d4c90c8b5a76dcd65144
clapota <33963998+clapota@users.noreply.github.com>
2019-05-10 14:02:54 +0200

    [+] Added sample code that work both on Linux and Windows normally, must put the static library for linux

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,26 @@
 ** Created by abel,
 */
 
-#include <irrlicht/irrlicht.h>
-#include "crossPlatform.hpp"
+#include <iostream>
+#include "irrlicht.h"
 
-int main(int ac, char **argv)
+int main(void)
 {
-    irr::IrrlichtDevice *device = irr::createDevice(irr::video::EDT_SOFTWARE,
-        irr::core::dimension2d<irr::u32 >(640, 480), 16, false, false, false, nullptr);
-    if (!device)
-        return 84;
-    device->setWindowCaption(L"Hello World");
+    std::cout << "Hello World !" << std::endl;
+    irr::IrrlichtDevice *device = irr::createDevice(irr::video::EDT_SOFTWARE, irr::core::dimension2d<irr::u32>(1920, 1080), 16, false, false, false, 0);
+    irr::video::IVideoDriver* driver = device->getVideoDriver();
+    irr::scene::ISceneManager* smgr = device->getSceneManager();
+    irr::gui::IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+    guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+        irr::core::rect<irr::s32>(10, 10, 260, 22), true);
+    device->setWindowCaption(L"Bonjour");
     while (device->run())
-    {}
+    {
+        driver->beginScene(true, true, irr::video::SColor(255, 100, 101, 140));
+        guienv->drawAll();
+        std::cout << "Running game" << std::endl;
+        driver->endScene();
+    }
     return 0;
 }
