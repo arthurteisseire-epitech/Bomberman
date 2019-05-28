8ba00559de66d4b365244d6aeb124be86f7d722d
arthur <arthur.teisseire@epitech.eu>
2019-05-28 09:32:10 +0200

    Refactor Bomb explode method

--- src/Entities/Bomb.cpp
+++ src/Entities/Bomb.cpp
@@ -11,80 +11,55 @@
 #include "BombBehaviour.hpp"
 
 ind::Bomb::Bomb(const ind::Position &position, ind::ORIENTATION rotation,
-    ind::Board &map, int power, std::function<void(Bomb *bomb)> onExplode, irr::scene::IMeshSceneNode *object) :
+                ind::Board &map, int power, std::function<void(Bomb *bomb)> onExplode,
+                irr::scene::IMeshSceneNode *object) :
     AbstractEntity(position, rotation, object), map(map), power(power), onExplode(std::move(onExplode))
 {
     auto *behaviour = new BombBehaviour(*this);
-    this->setBehaviour(static_cast<IBehaviour *>(behaviour));
+    setBehaviour(static_cast<IBehaviour *>(behaviour));
 }
 
 float ind::Bomb::getTime() const
 {
-    return this->timeBeforeExplosion;
+    return timeBeforeExplosion;
 }
 
 void ind::Bomb::decreaseTime(float n)
 {
-    this->timeBeforeExplosion -= n;
+    timeBeforeExplosion -= n;
 }
 
 void ind::Bomb::explode()
 {
-    bool can_left = true;
-    bool can_right = true;
-    bool can_up = true;
-    bool can_down = true;
+    const auto &pos = getPosition();
 
-    this->map.setAtCoord(this->getPosition(), EXPLOSION);
-    for (int i = 1; i <= this->power; ++i) {
-        if (static_cast<int>(this->getPosition().x) + i < this->map.getSize().x
-        && this->map.getInfoAtCoord(Position(this->getPosition().x + i, this->getPosition().y)) == BLOCKBREAKABLE && can_right) {
-            this->map.emptyTile(Position(this->getPosition().x + i,
-                this->getPosition().y));
-            this->map.setAtCoord(Position(this->getPosition().x + i,
-                this->getPosition().y), EXPLOSION);
-            can_right = false;
+    map.setAtCoord(pos, EXPLOSION);
+    explodeRow([pos](int i) { return Position(pos.x + i, pos.y); });
+    explodeRow([pos](int i) { return Position(pos.x - i, pos.y); });
+    explodeRow([pos](int i) { return Position(pos.x, pos.y + i); });
+    explodeRow([pos](int i) { return Position(pos.x, pos.y - i); });
+    onExplode(this);
+}
+
+bool ind::Bomb::explodeTile(const Position &pos)
+{
+    bool isEnd = false;
+
+    if (inMap(pos)) {
+        if (map.getInfoAtCoord(pos) == BLOCKBREAKABLE) {
+            map.emptyTile(pos);
+            isEnd = true;
             //TODO: put powerup ?
-        } else if (static_cast<int>(this->getPosition().x) + i < this->map.getSize().x && can_right) {
-            this->map.setAtCoord(Position(this->getPosition().x + i,
-                this->getPosition().y), EXPLOSION);
         }
-        if (static_cast<int>(this->getPosition().x) - i >= 0 && this->map.getInfoAtCoord(Position(this->getPosition().x - i, this->getPosition().y)) == BLOCKBREAKABLE && can_left) {
-            this->map.emptyTile(Position(this->getPosition().x - i,
-                this->getPosition().y));
-            this->map.setAtCoord(Position(this->getPosition().x - i,
-                this->getPosition().y), EXPLOSION);
-            can_left = false;
-            //TODO: put powerup ?
-        } else if (static_cast<int>(this->getPosition().x) - i >= 0 && can_left) {
-            this->map.setAtCoord(Position(this->getPosition().x - i,
-                this->getPosition().y), EXPLOSION);
-        }
-        if (static_cast<int>(this->getPosition().y) + i < this->map.getSize().y && this->map.getInfoAtCoord(Position(this->getPosition().x, this->getPosition().y + i)) == BLOCKBREAKABLE && can_up) {
-            this->map.emptyTile(Position(this->getPosition().x,
-                this->getPosition().y + i));
-            this->map.setAtCoord(Position(this->getPosition().x,
-                this->getPosition().y + i), EXPLOSION);
-            can_up = false;
-            //TODO: put powerup ?
-        } else if (static_cast<int>(this->getPosition().y) + i < this->map.getSize().y && can_up) {
-            this->map.setAtCoord(Position(this->getPosition().x,
-                this->getPosition().y + i), EXPLOSION);
-        }
-        if (static_cast<int>(this->getPosition().y) - i >= 0 && this->map.getInfoAtCoord(Position(this->getPosition().x, this->getPosition().y - i)) == BLOCKBREAKABLE && can_down) {
-            this->map.emptyTile(Position(this->getPosition().x,
-                this->getPosition().y - i));
-            this->map.setAtCoord(Position(this->getPosition().x,
-                this->getPosition().y - i), EXPLOSION);
-            can_down = false;
-            //TODO: put powerup ?
-        } else if (static_cast<int>(this->getPosition().y) - i >= 0 && can_down) {
-            this->map.setAtCoord(Position(this->getPosition().x,
-                this->getPosition().y - i), EXPLOSION);
-        }
+        map.setAtCoord(pos, EXPLOSION);
     }
-    this->onExplode(this);
+    return isEnd;
 }
 
 void ind::Bomb::draw()
 {}
+
+bool ind::Bomb::inMap(const Position &pos) const
+{
+    return pos.x >= 0 && pos.x < map.getSize().x && pos.y >= 0 && pos.y < map.getSize().y;
+}
--- src/Entities/Bomb.hpp
+++ src/Entities/Bomb.hpp
@@ -15,18 +15,29 @@ namespace ind {
 
     class Bomb : public AbstractEntity {
 
-        public:
-            Bomb(const Position &position, ORIENTATION rotation, Board &map, int power, std::function<void(Bomb *bomb)> onExplode, irr::scene::IMeshSceneNode *object);
-            ~Bomb() override = default;
-            void decreaseTime(float n);
-            void explode();
-            float getTime() const;
-            void draw() override;
+    public:
+        Bomb(const Position &position, ORIENTATION rotation, Board &map, int power,
+             std::function<void(Bomb *bomb)> onExplode, irr::scene::IMeshSceneNode *object);
+        ~Bomb() override = default;
+        void decreaseTime(float n);
+        void explode();
+        float getTime() const;
+        void draw() override;
+        bool inMap(const Position &pos) const;
 
-        private:
-            Board &map;
-            int power;
-            float timeBeforeExplosion = 1.0f;
-            std::function<void(Bomb *bomb)> onExplode;
+        template <typename Func>
+        void explodeRow(Func getPosAt)
+        {
+            for (int i = 1; i < power; ++i)
+                if (explodeTile(getPosAt(i)))
+                    return;
+        }
+        bool explodeTile(const ind::Position &pos);
+
+    private:
+        Board &map;
+        int power;
+        float timeBeforeExplosion = 1.0f;
+        std::function<void(Bomb *bomb)> onExplode;
     };
 }
