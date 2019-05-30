a158bfe134c1d202a2fad3c8ab32d5a1ef56e4ab
Taz <lucas.benghida@epitech.eu>
2019-05-30 02:47:56 +0200

    [+] Cross platform file system

---
+++ src/Services/FileSystemService.cpp
@@ -0,0 +1,14 @@
+#include "FileSystemService.hpp"
+
+services::FILE_STATUS services::FileSystemService::exists(std::string &path)
+{
+    struct stat info;
+
+    if (stat(path.c_str(), &info ) != 0 )
+        return FILE_STATUS::CANNOT_ACCESS;
+    else if (info.st_mode & S_IFDIR)
+        return FILE_STATUS::DIRECTORY;
+    else if (info.st_mode & S_IFREG)
+        return FILE_STATUS::FILE;
+    return FILE_STATUS::UNKNOW;
+}
