9076c8f86d146a78335a6dbe987469327560ce38
arthur <arthur.teisseire@epitech.eu>
2019-06-15 09:44:37 +0200

    Change c-style cast to static_cast in to2d

--- src/Utilities/to2d.cpp
+++ src/Utilities/to2d.cpp
@@ -12,8 +12,8 @@
 
 ind::Position ind::to2d(const irr::core::vector3df &pos3d)
 {
-    int x = (int) ((pos3d.X + TILE_SIZE / 2) / TILE_SIZE);
-    int y = (int) ((pos3d.Z + TILE_SIZE / 2) / TILE_SIZE);
+    int x = static_cast<int>((pos3d.X + TILE_SIZE / 2) / TILE_SIZE);
+    int y = static_cast<int>((pos3d.Z + TILE_SIZE / 2) / TILE_SIZE);
 
     return {x, y};
 }
